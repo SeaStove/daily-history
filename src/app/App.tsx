8efe07116d796ea50ddd4206d0d817646fab147e
Christian Stovall <seastove@gmail.com>
2024-07-11 01:46:18 -0400

    Updates

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,13 @@
-import { Heading, Center, Link, VStack } from "@chakra-ui/react";
+import {
+  Heading,
+  Center,
+  Link,
+  VStack,
+  Image,
+  Text,
+  Flex,
+  Box,
+} from "@chakra-ui/react";
 import useDocumentTitle from "../common/hooks/useDocumentTitle";
 
 function App() {
@@ -6,8 +15,22 @@ function App() {
 
   return (
     <Center bgColor={"brand.100"} w="100vw" h="100vh">
-      <VStack>
-        <Heading>Hi, here's some games:</Heading>
+      <VStack gap="5rem">
+        <Link
+          bgColor="#1D2B53"
+          color="#F9F7C9"
+          p={{ base: "2rem", md: "4rem" }}
+          borderRadius={"8px"}
+          fontSize={{ base: "2rem", md: "4rem" }}
+          href="http://www.dailyeruption.com/"
+          fontFamily="HONK"
+        >
+          <Flex alignItems={"center"}>
+            <Image src="eruption.png" alt="Volcano" w="64px" h="64px" />
+            Daily Eruption
+            <Image src="eruption.png" alt="Volcano" w="64px" h="64px" />
+          </Flex>
+        </Link>
         <Link
           bgColor="brand.300"
           color="gray.700"
@@ -18,6 +41,31 @@ function App() {
         >
           Endless History Game
         </Link>
+        <Box as="footer" textAlign="center" color="white">
+          <Text mb={2}>
+            <Link
+              href="https://github.com/seastove"
+              target="_blank"
+              rel="noopener noreferrer"
+              textDecoration={"underline"}
+            >
+              github.com/seastove
+            </Link>{" "}
+            |{" "}
+            <Link
+              href="https://seastove.games"
+              target="_blank"
+              rel="noopener noreferrer"
+              textDecoration={"underline"}
+            >
+              seastove.games
+            </Link>
+          </Text>
+          <Text>
+            &copy; {new Date().getFullYear()} Christian Stovall. All rights
+            reserved.
+          </Text>
+        </Box>
       </VStack>
     </Center>
   );
